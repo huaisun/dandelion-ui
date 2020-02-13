54a3b580d3d0f5b9d4e62de72de1629e3001422a
huaisun <420007900@qq.com>
2020-02-13 20:00:44 +0800

    完成上传页面的样式

--- src/views/domain/local/Local.vue
+++ src/views/domain/local/Local.vue
@@ -1,23 +1,52 @@
 <template>
-  <v-row dense>
-    <v-col cols="4" v-for="(item,index) in items" :key="index">
-      <v-card class="mx-auto" @click="cardClick(item)">
-        <div class="d-flex flex-no-wrap justify-space-between">
-          <v-avatar class="ma-3" size="75" tile>
-            <v-img :src="item.src"></v-img>
-          </v-avatar>
-          <div>
-            <v-card-title class="headline" v-text="item.title"></v-card-title>
+  <div>
+    <v-row class="left" dense>
+      <v-col cols="6" v-for="(item,index) in items" :key="index">
+        <v-card class="mx-auto" @click="cardClick(item)">
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <v-avatar class="ma-3" size="75" tile>
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
 
-            <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            </div>
           </div>
-        </div>
-      </v-card>
-    </v-col>
-    <v-dialog v-model="windowDialog" width="1080px">
-      <InnerDialog :flag="windowFlag" @closeDialog="closeInnerDialog"></InnerDialog>
-    </v-dialog>
-  </v-row>
+        </v-card>
+      </v-col>
+      <v-dialog v-model="windowDialog" width="1080px">
+        <InnerDialog :flag="windowFlag" @closeDialog="closeInnerDialog"></InnerDialog>
+      </v-dialog>
+    </v-row>
+    <div class="right">
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>导入文件</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-radio-group v-model="format">
+              <v-radio
+                v-for="(item, index) in items"
+                :key="index"
+                :label="item.flag"
+                color="red"
+                :value="item.flag"
+              ></v-radio>
+            </v-radio-group>
+            <v-file-input show-size label="File input"></v-file-input>
+            <v-btn color="primary">
+              <v-icon>cloud_upload</v-icon>
+              上传
+            </v-btn>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </div>
+  </div>
 </template>
 <script>
 import Chrome from "@/assets/photos/chrome.png";
@@ -36,7 +65,8 @@ export default {
       }
     ],
     windowDialog: false,
-    windowFlag: null
+    windowFlag: null,
+    format: ""
   }),
   methods: {
     cardClick(data) {
@@ -54,5 +84,20 @@ export default {
 <style scoped>
 .navigation-1 {
   background-image: linear-gradient(to bottom, #77a1d3, #79cbca);
+}
+.right {
+  margin-bottom: 30px;
+  padding: 0;
+  max-width: 293px;
+  position: absolute;
+  right: 0;
+  width: 100%;
+}
+
+.left {
+  float: left;
+  margin-right: 28px;
+  max-width: 614px;
+  width: 100%;
 }
 </style>
